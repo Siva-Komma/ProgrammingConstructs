1b204023b725deb67ea5db78030034790dc40e14
Siva-Komma <kommasivajyothi22@gmail.com>
2023-04-07 21:57:32 +0530

    Printing sting in reverse order

--- ProgrammingConstructs/Program.cs
+++ ProgrammingConstructs/Program.cs
@@ -11,7 +11,7 @@ namespace ProgrammingConstructs
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Programming Constructs");
-            Console.WriteLine("1.CheckTwoNumbers\n2.EvenorOdd\n3.Vote eligibility\n4.LargestNumber\n5.Eligibility Criteria\n6.WeekDay\n7.Arithmetic Operations\n8.PowerofTable\n9.SumofSquares\n10.Factorial\n11.FlipCoin");
+            Console.WriteLine("1.CheckTwoNumbers\n2.EvenorOdd\n3.Vote eligibility\n4.LargestNumber\n5.Eligibility Criteria\n6.WeekDay\n7.Arithmetic Operations\n8.PowerofTable\n9.SumofSquares\n10.Factorial\n11.FlipCoin\n12.Reverse Word");
             Console.WriteLine("Choose your Option: ");
             int option =Convert.ToInt32(Console.ReadLine());
 
@@ -49,6 +49,9 @@ namespace ProgrammingConstructs
                     break;
                 case 11:
                     FlipCoin.Coin();
+                    break;
+                case 12:
+                    ReverseWord.Word("Hello"); 
                     break;
             }
             Console.ReadLine();
---
+++ ProgrammingConstructs/ReverseWord.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingConstructs
+{
+    internal class ReverseWord
+    {
+        public static void Word(string stringInput)
+        {
+            string revword = "";
+            int length=stringInput.Length-1;
+
+            while(length>=0)
+            {
+                revword = revword + stringInput[length];
+                length--;
+            }
+            Console.WriteLine(revword +"\n");
+        }
+    }
+}
