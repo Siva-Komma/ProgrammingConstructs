079eb3ea1a9d9022b796db846ff5cd2ca45eec9d
Siva-Komma <kommasivajyothi22@gmail.com>
2023-04-07 20:01:21 +0530

    Calculating Square root of number range

--- ProgrammingConstructs/Program.cs
+++ ProgrammingConstructs/Program.cs
@@ -11,7 +11,7 @@ namespace ProgrammingConstructs
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Programming Constructs");
-            Console.WriteLine("1.CheckTwoNumbers\n2.EvenorOdd\n3.Vote eligibility\n4.LargestNumber\n5.Eligibility Criteria\n6.WeekDay\n7.Arithmetic Operations\n8.PowerofTable");
+            Console.WriteLine("1.CheckTwoNumbers\n2.EvenorOdd\n3.Vote eligibility\n4.LargestNumber\n5.Eligibility Criteria\n6.WeekDay\n7.Arithmetic Operations\n8.PowerofTable\n9.SumofSquares");
             Console.WriteLine("Choose your Option: ");
             int option =Convert.ToInt32(Console.ReadLine());
 
@@ -40,6 +40,9 @@ namespace ProgrammingConstructs
                     break;
                 case 8:
                     PowerofTable.Table();
+                    break;
+                case 9:
+                    SumofSquares.Square();
                     break;
             }
             Console.ReadLine();
---
+++ ProgrammingConstructs/SumofSquares.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingConstructs
+{
+    internal class SumofSquares
+    {
+        public static void Square()
+        {
+            int sum = 0, i=1;
+            Console.WriteLine("Enter Square range");
+            int num=Convert.ToInt32(Console.ReadLine());
+
+            while(i<=num)
+            {
+                sum += i * i;
+                Console.WriteLine(sum);
+                i++;
+            }
+        }
+    }
+}
